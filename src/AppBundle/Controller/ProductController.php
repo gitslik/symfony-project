f060dfc1290f7c685ef1dfe1e189642564fed316
slik <inwteyn@gmail.com>
2020-03-13 13:53:26 +0600

    add values for example

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -13,14 +13,20 @@ class ProductController extends Controller
      * @Route("/product", name="product-list")
      */
     public function indexAction(){
-        return $this->render('@App/product/index.html.twig');
+        $products = [];
+        for($i=1; $i<=10; $i++){
+            $products[]=rand(1,100);
+        }
+        return $this->render('@App/product/index.html.twig', ["products"=>$products]);
     }
 
     /**
-     * @Route("/show", name="show-product")
+     * @Route("/show/{id}", name="show-product", requirements={"id": "[0-9]+"})
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function showAction(){
-        return $this->render('@App/product/show.html.twig');
+    public function showAction($id){
+        return $this->render('@App/product/show.html.twig', ["id"=>$id]);
     }
 
 }
